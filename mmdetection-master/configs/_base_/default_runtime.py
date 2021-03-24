a8bb2db22efc665a291aedeceab22030d2dc4194
hanchenchen <13980209828@163.com>
2021-03-24 19:47:07 +0800

    use cascade rcnn

--- mmdetection-master/configs/_base_/default_runtime.py
+++ mmdetection-master/configs/_base_/default_runtime.py
@@ -11,6 +11,6 @@ custom_hooks = [dict(type='NumClassCheckHook')]
 
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-load_from = './checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth' # None
+load_from = None #'./checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth' # None
 resume_from = None # './work_dirs/faster_rcnn_r50_fpn_1x_coco/latest.pth'
 workflow = [('train', 1)]
